17d5b7cf5ab3ee675dcd65c37adc6cffc48dd2c8
Vyacheslav Lopachev <vmlopach@gmail.com>
2018-12-18 00:10:53 +0600

    refactoring

--- src/RevealWinners.java
+++ src/RevealWinners.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class RevealWinners {
     private Set<Integer> winNumbers = new LinkedHashSet<>();
-    private Set<Ticket> tickets = new HashSet<>();
+    private Set<Ticket> tickets;
     private int winNumber;
     private Map<Integer, Integer> countWinners = new HashMap<>();
 
@@ -29,23 +29,21 @@ public class RevealWinners {
     }
 
     public void printWinners() {
-        for (int i = 1; i <= winNumbers.size(); i++) {
+        for (int i = 0; i <= winNumbers.size(); i++) {
             countWinners.put(i, 0);
         }
-        for (Ticket ticket: tickets){
-            if (ticket.isWin()){
-                int countWinNumbers = ticket.getNumbersMatchTheWinning().size();
-                countWinners.put(countWinNumbers, countWinners.get(countWinNumbers) +1);
-            }
+        for (Ticket ticket : tickets) {
+            int countWinNumbers = ticket.getNumbersMatchTheWinning().size();
+            countWinners.put(countWinNumbers, countWinners.get(countWinNumbers) + 1);
         }
-        for (Map.Entry<Integer, Integer> pair: countWinners.entrySet()){
+        for (Map.Entry<Integer, Integer> pair : countWinners.entrySet()) {
             System.out.println(pair.getKey() + ": " + pair.getValue());
         }
     }
 
     private void matchWinNumbersInTickets() {
         for (Ticket ticket : tickets) {
-            if (ticket.getNumbers() != null && ticket.getNumbers().contains(winNumber)){
+            if (ticket.getNumbers() != null && ticket.getNumbers().contains(winNumber)) {
                 ticket.getNumbersMatchTheWinning().add(winNumber);
                 ticket.setWin(true);
             }
--- src/Runner.java
+++ src/Runner.java
@@ -2,11 +2,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Runner {
-    private static final int SIZE_RANGE_NUMBERS = 49;
-    private static final int COUNT_NUMBER_OF_PLAYED = 6;
+    private static final int SIZE_RANGE_NUMBERS = 36;
+    private static final int COUNT_NUMBER_OF_PLAYED = 5;
     private static final int COUNT_TICKETS = 1000000;
 
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
 
         GeneratorNumbersForTicket generator = new GeneratorNumbersForTicket(SIZE_RANGE_NUMBERS);
 
@@ -21,23 +23,16 @@ public class Runner {
 
         Wheel wheel = new Wheel(SIZE_RANGE_NUMBERS);
 
+
         for (int i = 1; i <= COUNT_NUMBER_OF_PLAYED; i++) {
             winners.setWinNumber(wheel.getWinNumber());
         }
 
         winners.printWinners();
 
+         long stopTime = System.currentTimeMillis();
 
-
-
-
-
-
-
-
-
-
-
+        System.out.println(stopTime - startTime);
 
     }
 
